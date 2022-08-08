48e47ae2c7cf2a4ac4c482356c6751759dd342c6
rakhmatov-kamoliddin <raxmatovkamoliddin746@gmail.com>
2022-08-08 20:53:06 +0500

    'kjjkj'

--- data/config.py
+++ data/config.py
@@ -8,9 +8,9 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
-
+CHANNELS=['-1001719663128']
 
 # ADMINS=['1282301807']
 # BOT_TOKEN='5396598765:AAGcyUdYGHWosR9bQJQVfQ86BIXoa2TVzJw'
 # ip='localhost'
-# CHANNELS=['-1001719663128']
+# 
