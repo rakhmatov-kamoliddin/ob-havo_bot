23290d1e07d02cbe43a500e5803d3873b1f013ee
rakhmatov-kamoliddin <raxmatovkamoliddin746@gmail.com>
2022-08-08 19:01:46 +0500

    'jghfdf'

--- data/config.py
+++ data/config.py
@@ -5,12 +5,12 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-# IP = env.str("ip")  # Xosting ip manzili
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+IP = env.str("ip")  # Xosting ip manzili
 
 
-ADMINS=['1282301807']
-BOT_TOKEN='5396598765:AAGcyUdYGHWosR9bQJQVfQ86BIXoa2TVzJw'
-ip='localhost'
-CHANNELS=['-1001719663128']
+# ADMINS=['1282301807']
+# BOT_TOKEN='5396598765:AAGcyUdYGHWosR9bQJQVfQ86BIXoa2TVzJw'
+# ip='localhost'
+# CHANNELS=['-1001719663128']
